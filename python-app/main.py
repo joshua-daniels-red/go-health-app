4fb9456a102adbeb95b5eeea7224143d50bc1304
Joshua D <joshua.daniels@redspace.com>
2025-09-09 11:13:44 -0300

    update processing to be done in batches

--- python-app/main.py
+++ python-app/main.py
@@ -5,9 +5,10 @@ import os
 
 BASE_URL = os.getenv("GO_SERVER_URL", "http://localhost:8080")
 
-def fetch_all_movies(page_size=2):
+def fetch_movie_batches(page_size=2):
     """
-    Generator that yields movies from /data endpoint in paginated chunks.
+    Generator that yields lists of movies from /data endpoint in paginated chunks.
+    Each yield is a batch (e.g., 2 movies).
     """
     page = 1
     while True:
@@ -17,31 +18,33 @@ def fetch_all_movies(page_size=2):
         items = resp.json()
         if not items:
             break
-        yield from items
+        yield items   
         page += 1
 
 
 def transform_imdb():
     results = []
-    for movie in fetch_all_movies():
-        old = movie["imdb"]
-        new = round(old + 0.1, 1)
-        results.append({
-            "id": movie["id"],
-            "change": f"imdb score will be changed from {old} to {new}"
-        })
+    for batch_num, batch in enumerate(fetch_movie_batches(), start=1):
+        for movie in batch:
+            old = movie["imdb"]
+            new = round(old + 0.1, 1)
+            results.append({
+                "id": movie["id"],
+                "change": f"imdb score will be changed from {old} to {new}"
+            })
     return results
 
 
 def transform_title():
     results = []
-    for movie in fetch_all_movies():
-        old = movie["movie_title"]
-        new = f"{old} (English)"
-        results.append({
-            "id": movie["id"],
-            "change": f"title will be changed from '{old}' to '{new}'"
-        })
+    for batch_num, batch in enumerate(fetch_movie_batches(), start=1):
+        for movie in batch:
+            old = movie["movie_title"]
+            new = f"{old} (English)"
+            results.append({
+                "id": movie["id"],
+                "change": f"title will be changed from '{old}' to '{new}'"
+            })
     return results
 
 
